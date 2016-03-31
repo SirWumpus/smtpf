2daaec6d58a7b143f200d0d71f283cff0fba27dc
Anthony Howe <achowe@snert.com>
2016-03-31 16:24:06 -0400

    update documentation and change sesssion_accept() linger on close from 0 to LINGER_ON_CLOSE to allow clients more time to collect last reply and reduce possible duplicates.

--- src/server.c
+++ src/server.c
@@ -339,7 +339,7 @@ session_accept(ServerSession *session)
 	(void) socketSetNonBlocking(session->client, 1);
 
 	/* Assume the client connection will drop more often than send QUIT. */
-	(void) socketSetLinger(session->client, 0);
+	(void) socketSetLinger(session->client, LINGER_ON_CLOSE);
 
 	switch ((unsigned)filterRun(data, filter_accept_table)) {
 	case SMTPF_DROP:
